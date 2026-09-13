7f4a5b8b829245f8dc60cc3e7e5e111eac413844
jacobs_matteo <matteo.jacobs@student.howest.be>
2026-09-13 14:22:35 -0400

    updated navigation logo to use inline SVG

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import logoMark from '../assets/logo-mark.svg'
 import hamburgerPlate from '../assets/hamburger-plate.svg'
 import './navigation.css'
+
+const LOGO_PATH =
+    'M9.998 29.3275V31.9936H0V21.551L9.998 29.3275ZM31.9936 31.9936H21.9956V29.3275L31.9936 21.551V31.9936ZM9.998 21.9956V25.9948L0 18.2184V14.2192L9.998 21.9956ZM31.9936 18.2184L21.9956 25.9948V21.9956L31.9936 14.2192V18.2184ZM31.9936 10.8865L21.9956 18.6629L15.9968 23.3287L9.998 18.6629L0 10.8865V6.88729L15.9968 19.3295L31.9936 6.88729V10.8865ZM31.9936 3.55462L15.9968 15.9968L0 3.55462V0L15.9968 11.9976L31.9936 0V3.55462Z'
 
 const TABLET_BREAKPOINT = 744
 
@@ -21,7 +23,14 @@ export default function Navigation() {
     return (
         <nav className={isOpen ? 'open' : 'closed'}>
             <NavLink to="/" className="logo">
-                <img src={logoMark} alt="Home" className="logo-image" />
+                <span className="logo-image logo-mark" role="img" aria-label="Home">
+                    <svg viewBox="0 0 32 32" width="32" height="32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path d={LOGO_PATH} fill="#F0F0F0" />
+                    </svg>
+                    <svg className="logo-mark-fill" viewBox="0 0 32 32" width="32" height="32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path d={LOGO_PATH} fill="#FF0000" />
+                    </svg>
+                </span>
                 <span className="logo-text">Home</span>
             </NavLink>
 
